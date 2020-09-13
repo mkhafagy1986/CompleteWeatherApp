af7b3a83d5a2ea88d009640c82ff4ded3cbc61dc
Mohamed Mahmoud Mahmoud <mkhafagy.c@ksu.edu.sa>
2020-09-13 17:02:47 +0300

    Access Location Automaticly

--- CompleteWeatherApp/CompleteWeatherApp/Views/CurrentWeatherPage.xaml.cs
+++ CompleteWeatherApp/CompleteWeatherApp/Views/CurrentWeatherPage.xaml.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -18,10 +18,41 @@ namespace CompleteWeatherApp.Views
         public CurrentWeatherPage()
         {
             InitializeComponent();
+            
+            GetCurrentLocation();
+            
             GetWeatherInfo();
+            GetForecast();
         }
 
-        public string Location = "Riyadh";
+        public string Location { get; set; } = "Jeddah";
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+
+        private async void GetCurrentLocation()
+        {
+            var request = new GeolocationRequest(GeolocationAccuracy.Best);
+
+            var location = await Geolocation.GetLocationAsync(request);
+
+            if (location != null)
+            {
+                Latitude = location.Latitude;
+                Longitude = location.Longitude;
+
+                Location = await GetCity(location);
+            }
+        }
+
+        private async Task<string> GetCity(Location location)
+        {
+            var place = await Geocoding.GetPlacemarksAsync(location);
+            var currentPlace = place?.FirstOrDefault();
+
+            if (currentPlace != null)
+                return $"{currentPlace.Locality},{currentPlace.CountryName}";
+            return null;
+        }
 
         private async void GetWeatherInfo()
         {
@@ -56,6 +87,60 @@ namespace CompleteWeatherApp.Views
             else
             {
                 await DisplayAlert("Weather Information","No weather information found","Ok");
+            }
+        }
+
+        private async void GetForecast()
+        {
+            var url = $"http://api.openweathermap.org/data/2.5/forecast?q={Location}&appid=5f1d9ce0e366e66fa383b1a8a6ff8892&units=metric";
+            var result = await ApiCaller.Get(url);
+
+            if (result.Successful)
+            {
+                try
+                {
+                    var forcastInfo = JsonConvert.DeserializeObject<ForecastInfo>(result.Response);
+
+                    List<List> allList = new List<List>();
+
+                    foreach (var list in forcastInfo.list)
+                    {
+                        //var date = DateTime.ParseExact(list.dt_txt, "yyyy-MM-dd hh:mm:ss", CultureInfo.InvariantCulture);
+                        var date = DateTime.Parse(list.dt_txt);
+
+                        if (date > DateTime.Now && date.Hour == 0 && date.Minute == 0 && date.Second == 0)
+                            allList.Add(list);
+                    }
+
+                    dayOneTxt.Text = DateTime.Parse(allList[0].dt_txt).ToString("dddd");
+                    dateOneTxt.Text = DateTime.Parse(allList[0].dt_txt).ToString("dd MMM");
+                    iconOneImg.Source = $"http://openweathermap.org/img/wn/{allList[0].weather[0].icon}@2x.png";
+                    tempOneTxt.Text = allList[0].main.temp.ToString("0");
+
+                    dayTwoTxt.Text = DateTime.Parse(allList[1].dt_txt).ToString("dddd");
+                    dateTwoTxt.Text = DateTime.Parse(allList[1].dt_txt).ToString("dd MMM");
+                    iconTwoImg.Source = $"http://openweathermap.org/img/wn/{allList[1].weather[0].icon}@2x.png";
+                    tempTwoTxt.Text = allList[1].main.temp.ToString("0");
+
+                    dayThreeTxt.Text = DateTime.Parse(allList[2].dt_txt).ToString("dddd");
+                    dateThreeTxt.Text = DateTime.Parse(allList[2].dt_txt).ToString("dd MMM");
+                    iconThreeImg.Source = $"http://openweathermap.org/img/wn/{allList[2].weather[0].icon}@2x.png";
+                    tempThreeTxt.Text = allList[2].main.temp.ToString("0");
+
+                    dayFourTxt.Text = DateTime.Parse(allList[3].dt_txt).ToString("dddd");
+                    dateFourTxt.Text = DateTime.Parse(allList[3].dt_txt).ToString("dd MMM");
+                    iconFourImg.Source = $"http://openweathermap.org/img/wn/{allList[3].weather[0].icon}@2x.png";
+                    tempFourTxt.Text = allList[3].main.temp.ToString("0");
+
+                }
+                catch (Exception ex)
+                {
+                    await DisplayAlert("Weather Info", ex.Message, "OK");
+                }
+            }
+            else
+            {
+                await DisplayAlert("Weather Info", "No forecast information found", "OK");
             }
         }
     }
